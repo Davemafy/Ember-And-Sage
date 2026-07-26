f7fe8ce5a878339fb314eb7a76a4fc747bb3de28
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-07-26 14:33:43 +0100

    Setup app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// app.tsx
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App; 

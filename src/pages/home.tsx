2187a2db40c125d0860be24e40985ca50094e285
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-07-27 15:49:57 +0100

    Update file path for sections

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,9 @@
-import HomeHero from "../components/sections/home-hero";
+import HomeHero from "../components/sections/home/home-hero";
 import Footer from "../components/layout/footer";
-import HomeMenuPreview from "../components/sections/home-menu-preview";
-import HomeExperience from "../components/sections/home-experience";
-import HomeStory from "../components/sections/home-story";
-import HomeGalleryPreview from "../components/sections/home-gallery-preview";
+import HomeMenuPreview from "../components/sections/home/home-menu-preview";
+import HomeExperience from "../components/sections/home/home-experience";
+import HomeStory from "../components/sections/home/home-story";
+import HomeGalleryPreview from "../components/sections/home/home-gallery-preview";
 
 const Home = () => {
   return (

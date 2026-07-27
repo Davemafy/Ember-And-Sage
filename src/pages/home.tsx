429ec7b9321f9e2b18e6c10f7579a79b81574251
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-07-27 15:18:17 +0100

    Update home with new sections

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import HomeHero from "../components/sections/home-hero";
 import Footer from "../components/layout/footer";
 import HomeMenuPreview from "../components/sections/home-menu-preview";
+import HomeExperience from "../components/sections/home-experience";
+import HomeStory from "../components/sections/home-story";
 
 const Home = () => {
   return (
@@ -8,6 +10,8 @@ const Home = () => {
       <main>
         <HomeHero />
         <HomeMenuPreview />
+        <HomeExperience />
+        <HomeStory />
       </main>
 
       <Footer />

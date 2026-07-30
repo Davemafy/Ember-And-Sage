5fda2819a44e4e3c3823c5a99a57a41bdb430b0d
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-07-30 12:28:18 +0100

    Refine home hero section

--- src/components/sections/home/home-hero.tsx
+++ src/components/sections/home/home-hero.tsx
@@ -1,12 +1,25 @@
-import { Play } from "lucide-react";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Calendar,
+  ChevronDown,
+  Clock,
+  Dot,
+  DotIcon,
+  PersonStanding,
+  Play,
+  ShieldCheck,
+  User,
+} from "lucide-react";
 import Button from "../../ui/button";
 import heroBanner from "@/assets/img/home-hero.png";
+import dotIcon from "@/assets/img/dot.svg";
 
 const HomeHero = () => {
   return (
-    <section className={` grid gap-4 text-warm-ivory `}>
+    <section className={` grid text-warm-ivory pb-4 `}>
       <div
-        className={`relative isolate  min-h-[550px] round-bottom overflow-clip  bg-near-black px-4 pb-8 pt-6`}
+        className={`relative isolate min-h-[560px] round-bottom overflow-clip  bg-near-black px-4 pb-8 pt-6`}
       >
         <img
           src={heroBanner}
@@ -34,6 +47,70 @@ const HomeHero = () => {
               </Button>
               <p className="text-[15px] font-light">Watch our story</p>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-7">
+          <div className="card rounded-2xl -mt-30 relative w-full bg-[#f9f4f1] shadow-xl shadow-[#d6d2d1] p-5 pt-4 px-8 text-text-primary">
+            <h2 className="mt-3 font-serif font-semibold text-[24px] leading-none">
+              Reserve Your Table
+            </h2>
+            <p className="0 flex items-center gap-2 mt-2 text-sm font-light text-text-muted">
+              <img
+                src={dotIcon}
+                alt="dot"
+                className="text-[60px] align-text-top h-1.25 text-burnt-orange"
+              />
+              <span className="text-xs">Quick and Easy</span>
+            </p>
+
+            <div className="mt-5 space-y-3">
+              <div className="rounded-xl flex items-center gap-2 bg-white border border-border-light px-4 py-3">
+                <Calendar size={16} />
+                <p className="mt-1 font-medium text-[13px] -translate-y-[2px]">
+                  Select Date
+                </p>
+                <ChevronDown size={20} className="ml-auto text-gray-500" />
+              </div>
+              <div className="rounded-xl flex items-center gap-2 bg-white border border-border-light px-4 py-3">
+                <Clock size={16} />
+                <p className="mt-1 font-medium text-[13px] -translate-y-[2px]">
+                  Select Time
+                </p>
+                <ChevronDown size={20} className="ml-auto text-gray-500" />
+              </div>
+              <div className="rounded-xl flex items-center gap-2 bg-white border border-border-light px-4 py-3">
+                <User size={16} />
+                <p className="mt-1 font-medium text-[13px] -translate-y-[2px]">
+                  2 Guests
+                </p>
+                <ChevronDown size={20} className="ml-auto text-gray-500" />
+              </div>
+            </div>
+
+            <div className="mt-5">
+              <Button
+                className=" text-xs h-12 rounded-md font-regular  flex items-center "
+                fullWidth
+              >
+                <span className="ml-auto">Find a Table</span>
+                <ArrowRight className="ml-auto" size={18} />
+              </Button>
+            </div>
+            <div className="flex items-center mt-6  gap-4">
+              <p className=" flex items-center  gap-2  text-sm font-light text-text-muted">
+                <ShieldCheck size={16} className="text-burnt-orange" />
+                <span className="text-[11px] whitespace-nowrap">
+                  {" "}
+                  No booking fees.
+                </span>
+              </p>
+              <img src={dotIcon} alt="dot" className=" h-1 text-burnt-orange" />
+              <p className="flex items-center gap-2 text-sm translate-px font-light text-text-muted">
+                <span className="text-[11px] whitespace-nowrap">
+                  Instant Confirmation
+                </span>
+              </p>
           </div>
         </div>
       </div>

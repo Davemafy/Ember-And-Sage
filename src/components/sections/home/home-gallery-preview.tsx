8f07c485c96063225c35e271f010aa76e5e27647
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-08-04 14:36:55 +0100

    Refine home gallery preview section

--- src/components/sections/home/home-gallery-preview.tsx
+++ src/components/sections/home/home-gallery-preview.tsx
@@ -1,22 +1,31 @@
 import { ArrowRight } from "lucide-react";
 import Button from "../../ui/button";
+import Gallery1 from "@/assets/img/gallery-1.png";
+import Gallery2 from "@/assets/img/gallery-2.png";
+import Gallery3 from "@/assets/img/gallery-3.png";
+import Gallery4 from "@/assets/img/gallery-4.png";
+import ReserveTable from "@/assets/img/reserve-table.png";
 
 const galleryItems = [
   {
     title: "Main Dining",
     label: "Atmosphere",
+    image: Gallery1,
   },
   {
     title: "Chef’s Counter",
     label: "Experience",
+    image: Gallery2,
   },
   {
     title: "Private Table",
     label: "Events",
+    image: Gallery3,
   },
   {
     title: "Signature Plates",
     label: "Cuisine",
+    image: Gallery4,
   },
 ];
 
@@ -39,12 +48,13 @@ const HomeGalleryPreview = () => {
 
       <div className="-mx-4 mt-1 flex gap-4 overflow-y-auto px-4">
         {galleryItems.map((item) => (
-          <article key={item.title} className={`h-40 w-70 shrink-0 rounded-xl bg-black`}></article>
+          <img key={item.title} src={item.image} alt={item.title} className={`h-40 w-80 shrink-0 rounded-xl bg-black`}></img>
         ))}
       </div>
 
-      <div className="bg-near-black mt-8 rounded-xl p-4 pb-8">
-        <h2 className="mt-4 text-white  max-w-[250px] font-serif text-[25px] leading-[0.95] font-medium tracking-[-0.03em]">
+      <div className="bg-near-black relative isolate mt-8 rounded-xl p-4 pb-8 overflow-clip">
+        <img src={ReserveTable} alt="Reserve Table" className="absolute -z-1 top-0 left-0 w-full h-full object-cover" />
+        <h2 className="mt-4 max-w-[250px] font-serif text-[25px] leading-[0.95] font-medium tracking-[-0.03em] text-white">
           Plan something special. We'll take care of the rest.
         </h2>
         <Button

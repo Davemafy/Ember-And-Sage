cd8d0b01d3313e9e2a6cae4062762c2d736fa101
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-08-05 22:27:56 +0100

    Refine menu hero

--- src/components/sections/menu/menu-hero.tsx
+++ src/components/sections/menu/menu-hero.tsx
@@ -1,7 +1,17 @@
+import menuBanner from "../../../assets/img/menu-banner.png";
+
 const MenuHero = () => {
   return (
-    <section className="bg-near-black px-4 pb-8 pt-8 text-warm-ivory">
-      <p className="text-xs font-semibold uppercase tracking-[0.24em] text-burnt-orange">
+    <section className="bg-near-black relative isolate px-4 pt-8 pb-40 text-white">
+      <img
+        src={menuBanner}
+        alt="Menu Banner"
+        className="bg-soft-black absolute top-0 left-0 -z-2 h-full w-full  object-cover"
+      />
+
+      <div className="pointer-events-none absolute inset-0 -z-1 bg-gradient-to-b from-black via-black/50 to-transparent"></div>
+
+      <p className="text-burnt-orange text-xs font-semibold tracking-[0.24em] uppercase">
         Our Menu
       </p>
 
@@ -9,12 +19,10 @@ const MenuHero = () => {
         Explore flavors crafted with passion.
       </h1>
 
-      <p className="mt-5 max-w-[310px] text-[15px] leading-7 text-warm-ivory/70">
-        From timeless classics to seasonal creations, every dish is a
-        celebration of taste, care, and exceptional ingredients.
+      <p className="text-warm-ivory mt-5 max-w-[26ch] text-[15px] leading-7">
+        From timeless classics to seasonal creations, every dish is a celebration of taste, care,
+        and exceptional ingredients.
       </p>
-
-      <div className="mt-8 h-[280px] rounded-[30px] bg-soft-black" />
     </section>
   );
 };
